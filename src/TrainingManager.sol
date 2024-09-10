c8d0a990829f9ddb3e175d70322808f5b687658d
alexnicita <alexandernicita@gmail.com>
2024-09-10 16:27:22 -0400

    fix: ints, emit, getters

--- src/TrainingManager.sol
+++ src/TrainingManager.sol
@@ -3,18 +3,19 @@
 pragma solidity ^0.8.19;
 
 import {ITrainingManager} from "./interfaces/ITrainingManager.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract TrainingManager is ITrainingManager {
 
-    mapping(string trainingRunId => TrainingRunStatus status) private trainingRunStatuses;
+    mapping(uint256 trainingRunId => TrainingRunStatus status) private trainingRunStatuses;
     mapping(address computeNodeAccount => string ipAddress) private registeredComputeNodes;
-    mapping(string trainingRunId => address[] computeNodes) private trainingRunComputeNodes;
+    mapping(uint256 trainingRunId => address[] computeNodes) private trainingRunComputeNodes;
     mapping(address computeNodeAccount => bytes[] attestations) private computeAttestations;
 
     string private _name;
     uint256 private _budget;
     uint256 public trainingRunIdCount;
+
+    event EndTrainingRun(uint256 trainingRunId);
 
     /**
      * @dev Sets the values for {name} and {budget}.
@@ -35,22 +36,21 @@ contract TrainingManager is ITrainingManager {
     /**
      * @dev Returns the budget for the training run
      */
-    function symbol() public view virtual returns (uint256 memory) {
+    function budget() public view virtual returns (uint256 memory) {
         return _budget;
     }
 
     function registerTrainingRun() external returns (string) {
         trainingRunIdCount++;
-        trainingRunId = Strings.toString(trainingRunIdCount);
-        trainingRunStatuses[trainingRunId] = TrainingRunStatus.Registered;
-        return trainingRunId;
+        trainingRunStatuses[trainingRunIdCount] = TrainingRunStatus.Registered;
+        return trainingRunIdCount;
     }
 
-    function getTrainingRunStatus(string trainingRunId) external returns (TrainingRunStatus) {
+    function getTrainingRunStatus(uint256 trainingRunId) external returns (TrainingRunStatus) {
         return trainingRunStatuses[trainingRunId];
     }
 
-    function registerComputeNode(address account, string ipAddress, string trainingRunId) external returns (bool) {
+    function registerComputeNode(address account, string ipAddress, uint256 trainingRunId) external returns (bool) {
         registeredComputeNodes[account] = ipAddress;
         trainingRunComputeNodes[trainingRunId].push(account);
         return true;
@@ -61,11 +61,11 @@ contract TrainingManager is ITrainingManager {
         return false;
     }
 
-    function startTrainingRun(string trainingRunId) external returns (bool) {
+    function startTrainingRun(uint256 trainingRunId) external returns (bool) {
         trainingRunStatuses[trainingRunId] = TrainingRunStatus.Running;
     }
 
-    function submitAttestation(address account, string trainingRunId, bytes attestation) external returns (bool) {
+    function submitAttestation(address account, uint256 trainingRunId, bytes attestation) external returns (bool) {
         bool doesTrainingRunContainNodeAddress = false;
         for (uint i = 0; i < trainingRunComputeNodes[trainingRunId].length; i++) {
             if (account == trainingRunComputeNodes[trainingRunId][i]) {
@@ -78,8 +78,17 @@ contract TrainingManager is ITrainingManager {
         return true;
     }
 
-    function endTrainingRun(string trainingRunId) external returns (bool) {
+    function getComputeNodesForTrainingRun(uint256 trainingRunId) returns (address[]) {
+        return trainingRunComputeNodes[trainingRunId];
+    }
+
+    function getAttestationsForComputeNode(address account) returns (bytes[]) {
+        return computeAttestations[account];
+    }
+
+    function endTrainingRun(uint256 trainingRunId) external returns (bool) {
          trainingRunStatuses[trainingRunId] = TrainingRunStatus.Done;
+         emit EndTrainingRun(trainingRunId);
     }
 
 }
