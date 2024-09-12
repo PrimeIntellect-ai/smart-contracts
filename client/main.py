998087b51a5782de9bac7d6c1063621ee45f68b7
alexnicita <alexandernicita@gmail.com>
2024-09-12 16:34:07 -0400

    writing code

--- client/main.py
+++ client/main.py
@@ -13,7 +13,7 @@ else:
 
 ### 2) INITIALIZE CONNECTION TO DEPLOYED CONTRACT
 
-abi_json_path = "../out/PrimeToken.sol/PrimeToken.json"
+abi_json_path = "../out/TrainingManager.sol/TrainingManager.json"
 with open(abi_json_path, "r") as f:
     abi = json.load(f)
 
@@ -26,11 +26,11 @@ contract = web3.eth.contract(address=contract_address, abi=abi['abi'])
 def get_name():
     return contract.functions.name().call()
 
-def get_symbol():
-    return contract.functions.symbol().call()
+def get_budget():
+    return contract.functions.budget().call()
 
 print("Name", get_name())
-print("Symbol", get_symbol())
+print("Budget", get_budget())
 
 """
 % python3 -i main.py
@@ -38,3 +38,36 @@ Connected to Anvil
 Name PrimeIntellectReward
 Symbol PRIME
 """   
+
+# register: model trainer
+pub_key = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
+pri_key = "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
+chain_id = web3.eth.chain_id
+caller = pub_key
+nonce = web3.eth.get_transaction_count(caller)
+call_function = contract.functions.registerTrainingRun().build_transaction({"chainId": chain_id, "from": caller, "nonce": nonce})
+signed_tx = web3.eth.account.sign_transaction(call_function, private_key=pri_key)
+send_tx = web3.eth.send_raw_transaction(signed_tx.raw_transaction)
+tx_receipt = web3.eth.wait_for_transaction_receipt(send_tx)
+# print(tx_receipt)
+run_id = contract.functions.registerTrainingRun().call()
+print("Registered training run ", run_id)
+
+# register: compute node
+node_1_pub_key = "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"
+node_1_priv_key = "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"
+node_1_ip_address = "test_ip_1"
+caller = node_1_pub_key
+nonce = web3.eth.get_transaction_count(caller)
+call_function = contract.functions.registerComputeNode(node_1_pub_key, node_1_ip_address, run_id).build_transaction({"chainId": chain_id, "from": caller, "nonce": nonce})
+signed_tx = web3.eth.account.sign_transaction(call_function, private_key=node_1_priv_key)
+send_tx = web3.eth.send_raw_transaction(signed_tx.raw_transaction)
+tx_receipt = web3.eth.wait_for_transaction_receipt(send_tx)
+
+# start: model trainer
+
+# train: compute node
+
+# end: model trainer
+
+# emit (?)
--- src/TrainingManager.sol
+++ src/TrainingManager.sol
@@ -6,8 +6,9 @@ import {ITrainingManager} from "./interfaces/ITrainingManager.sol";
 
 contract TrainingManager is ITrainingManager {
 
-    mapping(uint256 trainingRunId => TrainingRunStatus status) private trainingRunStatuses;
+    mapping(uint256 trainingRunId => ModelStatus status) private trainingRunStatuses;
     mapping(address computeNodeAccount => string ipAddress) private registeredComputeNodes;
+    mapping(address computeNodeAccount => bool validNode) private registeredValidComputeNodes;
     mapping(uint256 trainingRunId => address[] computeNodes) private trainingRunComputeNodes;
     mapping(address computeNodeAccount => bytes[] attestations) private computeAttestations;
 
@@ -21,7 +22,7 @@ contract TrainingManager is ITrainingManager {
      * @dev Sets the values for {name} and {budget}.
      *
      */
-    constructor(string memory name_, uint256 memory budget_) {
+    constructor(string memory name_, uint256 budget_) {
         _name = name_;
         _budget = budget_;
     }
@@ -36,36 +37,37 @@ contract TrainingManager is ITrainingManager {
     /**
      * @dev Returns the budget for the training run
      */
-    function budget() public view virtual returns (uint256 memory) {
+    function budget() public view virtual returns (uint256) {
         return _budget;
     }
 
-    function registerTrainingRun() external returns (string) {
+    function registerTrainingRun() external returns (uint256) {
         trainingRunIdCount++;
-        trainingRunStatuses[trainingRunIdCount] = TrainingRunStatus.Registered;
+        trainingRunStatuses[trainingRunIdCount] = ModelStatus.Registered;
         return trainingRunIdCount;
     }
 
-    function getTrainingRunStatus(uint256 trainingRunId) external returns (TrainingRunStatus) {
+    function getTrainingRunStatus(uint256 trainingRunId) external view returns (ModelStatus) {
         return trainingRunStatuses[trainingRunId];
     }
 
-    function registerComputeNode(address account, string ipAddress, uint256 trainingRunId) external returns (bool) {
+    function registerComputeNode(address account, string memory ipAddress, uint256 trainingRunId) external returns (bool) {
         registeredComputeNodes[account] = ipAddress;
+        registeredValidComputeNodes[account] = true;
         trainingRunComputeNodes[trainingRunId].push(account);
         return true;
     }
 
-    function isComputeNodeValid(address account) external returns (bool) {
-        if (registeredComputeNodes[account].isValue) return true;
-        return false;
+    function isComputeNodeValid(address account) external view returns (bool) {
+        return registeredValidComputeNodes[account];
     }
 
     function startTrainingRun(uint256 trainingRunId) external returns (bool) {
-        trainingRunStatuses[trainingRunId] = TrainingRunStatus.Running;
+        trainingRunStatuses[trainingRunId] = ModelStatus.Running;
+        return true;
     }
 
-    function submitAttestation(address account, uint256 trainingRunId, bytes attestation) external returns (bool) {
+    function submitAttestation(address account, uint256 trainingRunId, bytes memory attestation) external returns (bool) {
         bool doesTrainingRunContainNodeAddress = false;
         for (uint i = 0; i < trainingRunComputeNodes[trainingRunId].length; i++) {
             if (account == trainingRunComputeNodes[trainingRunId][i]) {
@@ -73,22 +75,23 @@ contract TrainingManager is ITrainingManager {
                 break;
             }
         }
-        if (!doesTrainingRunContainNodeAddress]) return false;
-        computeAttestations[computeNodeAccount].push(attestation);
+        if (!doesTrainingRunContainNodeAddress) return false;
+        computeAttestations[account].push(attestation);
         return true;
     }
 
-    function getComputeNodesForTrainingRun(uint256 trainingRunId) returns (address[]) {
+    function getComputeNodesForTrainingRun(uint256 trainingRunId) external view returns (address[] memory) {
         return trainingRunComputeNodes[trainingRunId];
     }
 
-    function getAttestationsForComputeNode(address account) returns (bytes[]) {
+    function getAttestationsForComputeNode(address account) external view returns (bytes[] memory) {
         return computeAttestations[account];
     }
 
     function endTrainingRun(uint256 trainingRunId) external returns (bool) {
-         trainingRunStatuses[trainingRunId] = TrainingRunStatus.Done;
+         trainingRunStatuses[trainingRunId] = ModelStatus.Done;
          emit EndTrainingRun(trainingRunId);
+         return true;
     }
 
 }
